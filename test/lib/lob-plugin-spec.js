2163369ef88a81f0858603b5c815152e193b1871
Brad Leupen <brad@entrinsik.com>
2015-06-03 12:35:34 -0400

    added size

--- test/lib/lob-plugin-spec.js
+++ test/lib/lob-plugin-spec.js
@@ -9,10 +9,10 @@ var P = $.sequelize.Promise;
 
 describe('LobPlugin', function () {
     it('should create a lob', function () {
-        return $.sequelize.createLob()
+        return $.sequelize.lobCreate()
             .then(function (lob) {
                 should.exist(lob);
-                return $.sequelize.unlinkLob(lob);
+                return $.sequelize.lobUnlink(lob);
             });
     });
 
@@ -20,14 +20,14 @@ describe('LobPlugin', function () {
         var lobId;
 
         beforeEach(function () {
-            return $.sequelize.createLob()
+            return $.sequelize.lobCreate()
                 .then(function (res) {
                     lobId = res;
                 });
         });
 
         afterEach(function () {
-            return $.sequelize.unlinkLob(lobId);
+            return $.sequelize.lobUnlink(lobId);
         });
 
         it('should write a stream to it', function () {
@@ -37,7 +37,7 @@ describe('LobPlugin', function () {
                 this.push();
             };
 
-            return $.sequelize.writeLob(lobId, readStream);
+            return $.sequelize.lobWrite(lobId, readStream);
         });
 
         it('should read a stream from it', function () {
@@ -58,9 +58,9 @@ describe('LobPlugin', function () {
                 return Buffer.concat(this.bufs);
             };
 
-            return $.sequelize.writeLob(lobId, readStream)
+            return $.sequelize.lobWrite(lobId, readStream)
                 .then(function () {
-                    return $.sequelize.readLob(lobId, writeStream);
+                    return $.sequelize.lobRead(lobId, writeStream);
                 })
                 .then(function () {
                     writeStream.result().toString().should.equal('this is a test');
@@ -85,15 +85,31 @@ describe('LobPlugin', function () {
                 return Buffer.concat(this.bufs);
             };
 
-            return $.sequelize.writeLob(lobId, readStream)
+            return $.sequelize.lobWrite(lobId, readStream)
                 .then(function(){
-                    return $.sequelize.truncateLob(lobId);
+                    return $.sequelize.lobTruncate(lobId);
                 })
                 .then(function(){
-                    return $.sequelize.readLob(lobId, writeStream);
+                    return $.sequelize.lobRead(lobId, writeStream);
                 })
                 .then(function() {
                     writeStream.bufs.should.have.length(0);
+                });
+        });
+
+        it('should get the size', function () {
+            var readStream = new stream.Readable();
+            readStream._read = function() {
+                this.push('this is a test');
+                this.push();
+            };
+
+            return $.sequelize.lobWrite(lobId, readStream)
+                .then(function () {
+                    return $.sequelize.lobSize(lobId);
+                })
+                .then(function (size) {
+                    size.should.equal('this is a test'.length);
                 });
         });
     });
